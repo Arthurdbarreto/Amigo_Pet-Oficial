61a3480edd103a9db8dbc65f4d8988cff6de288d
Arthur da Silva Barreto <arthurbarreto851@gmail.com>
2026-06-13 18:34:55 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 ﻿const { Sequelize } = require('sequelize');
 const sequelize = require('../config/db');
 
+const cors = require('cors');
+app.use(cors());
+
+
 const User = require('./user')(sequelize);
 const Tutor = require('./tutor')(sequelize);
 const Pet = require('./pet')(sequelize);
