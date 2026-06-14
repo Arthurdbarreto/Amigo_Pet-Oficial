9fe6dc6e637043adc6678279f52de503a22f14ca
Arthur da Silva Barreto <arthurbarreto851@gmail.com>
2026-06-13 22:19:51 -0300

    Update product.js

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -2,27 +2,11 @@
 
 module.exports = (sequelize) => {
   return sequelize.define('Product', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    descricao: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    preco: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false
-    },
-    estoque: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0
-    }
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nome: { type: DataTypes.STRING, allowNull: false },
+    descricao: { type: DataTypes.TEXT, allowNull: false },
+    preco: { type: DataTypes.DECIMAL(10,2), allowNull: false },
+    estoque: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 }
   }, { timestamps: true });
 };
+
