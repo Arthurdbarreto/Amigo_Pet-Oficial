f9b6ee3de55cce7f0924a9b6d41bb00c3afbba54
Arthur da Silva Barreto <arthurbarreto851@gmail.com>
2026-06-13 22:19:05 -0300

    Update service.js

--- backend/src/models/service.js
+++ backend/src/models/service.js
@@ -1,28 +1,10 @@
-﻿const { Sequelize } = require('sequelize');
-const sequelize = require('../config/db');
+const { DataTypes } = require('sequelize');
 
-const User = require('./user')(sequelize);
-const Tutor = require('./tutor')(sequelize);
-const Pet = require('./pet')(sequelize);
-const Service = require('./service')(sequelize);
-const Product = require('./product')(sequelize);
-const Appointment = require('./appointment')(sequ
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    descricao: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    preco: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false
-    }
+module.exports = (sequelize) => {
+  return sequelize.define('Service', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nome: { type: DataTypes.STRING, allowNull: false },
+    descricao: { type: DataTypes.TEXT, allowNull: false },
+    preco: { type: DataTypes.DECIMAL(10,2), allowNull: false }
   }, { timestamps: true });
 };
