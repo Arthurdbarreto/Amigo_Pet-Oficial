9d7adfd76c73880bbdb42e34b108d723d7209d6c
Arthur da Silva Barreto <arthurbarreto851@gmail.com>
2026-06-13 21:48:19 -0300

    Create index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,29 @@
+const sequelize = require('../config/db');
+const User = require('./user')(sequelize);
+const Tutor = require('./tutor')(sequelize);
+const Pet = require('./pet')(sequelize);
+const Service = require('./service')(sequelize);
+const Product = require('./product')(sequelize);
+const Appointment = require('./appointment')(sequelize);
+
+// Associações
+Tutor.hasMany(Pet, { foreignKey: 'tutorId', onDelete: 'CASCADE' });
+Pet.belongsTo(Tutor, { foreignKey: 'tutorId' });
+
+Tutor.hasMany(Appointment, { foreignKey: 'tutorId', onDelete: 'CASCADE' });
+Pet.hasMany(Appointment, { foreignKey: 'petId', onDelete: 'CASCADE' });
+Service.hasMany(Appointment, { foreignKey: 'serviceId', onDelete: 'CASCADE' });
+
+Appointment.belongsTo(Tutor, { foreignKey: 'tutorId' });
+Appointment.belongsTo(Pet, { foreignKey: 'petId' });
+Appointment.belongsTo(Service, { foreignKey: 'serviceId' });
+
+module.exports = {
+  sequelize,
+  User,
+  Tutor,
+  Pet,
+  Service,
+  Product,
+  Appointment
+};
